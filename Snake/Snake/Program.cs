2c390fb70bfdf1ab5feada446e909ca23d4bbdca
NicoleAAs <aasakuyla@gmail.com>
2020-08-26 04:06:44 +0300

    Perechislenije

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -18,7 +18,11 @@ namespace Snake
             leftLine.Drow();
             rightLine.Drow();
 
-            Console.ReadLine();
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+
+            Console.Read();
         }
 
     }
