ed117e553e24854135c30e359d1fd17054c7789f
NicoleAAs <aasakuyla@gmail.com>
2020-08-26 03:15:33 +0300

    Line

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,43 +13,13 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine line = new HorizontalLine(2, 4, 5, '*');
+            line.Drow();
 
-            int x = numList[0], y = numList[1], z = numList[2];
-
-            foreach (int i in numList) Console.WriteLine(i);
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            VerticalLine line2 = new VerticalLine(5, 3, 5, '#');
+            line2.Drow();
 
             Console.ReadLine();
-        }
-
-        public static void Func1(int value) {
-
-        }
-
-        public static void Func2(int value) {
-            value++;
-        }
-
-        public static void Func3(int x) {
-            x++;
-        }
-
-        public static void Move(Point p, int dx, int dy) {
-            p.x = p.x + dx;
-            p.y = p.y + dx;
-        }
-
-        public static void Reset(Point p) {
-            p = new Point(0, 0, '\0');
         }
 
     }
